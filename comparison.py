39edeff540e2ac9ac4f72e786b6b266542235cc6
diagonali-de <jamalialireza793@gmail.com>
2026-07-30 16:16:35 +0330

    Add files via upload
    python basic

---
+++ comparison.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 12:18:20 2026
+
+@author: ZST25-3
+"""
+
+a=2
+b=4
+c=3
+d=5
+print(a==b)
+print(c!=d)
+print(b>d)
+print(c<a)
+print(a<=b)
+print(d>=a)
---
+++ operational.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 12:15:05 2026
+
+@author: ZST25-3
+"""
+
+a=2
+b=4
+c=3
+d=5
+print(a+b)
+print(a-b)
+print(a*c)
+print(a/d)
+print(d**a)
+
