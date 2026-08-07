52f1d234efc10530160d7b63b2754dbf3e3634fb
alireza <jamalialireza793@yahoo.com>
2026-08-07 13:58:31 +0330

    Add new Python files

---
+++ string.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 15:50:01 2026
+
+@author: ZST25-3
+"""
+
+name='david'
+job='engineer'
+nationality='italien'
+
+
+print(name)
+print(job)
+print(nationality)
+
+print(type(name))
+print(type(job))
+print(type(nationality))
+
+print(len(name))
+print(len(job))
+print(len(nationality))
